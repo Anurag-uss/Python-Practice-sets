2710d49f1a192fed4f466caa8885e9fdad6c6ced
Anurag <Kumaranurag063276@gmail.com>
2026-09-08 09:59:04 +0530

    Added list folder and

---
+++ List/lists.py
@@ -0,0 +1,7 @@
+# To initiate a list
+friends =["apple","orange",5 ,343.22,False,"Anurag"]
+
+# To print the output
+print(friends[0])    #will give the output as "apple"
+
+#lists are muteable (can be modified as compaierd to the strings)
